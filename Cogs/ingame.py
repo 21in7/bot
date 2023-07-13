4f61bb334a5f36519cf4fdf29c9f0dc8d648bc44
21in7 <skawksmsduzo@gmail.com>
2023-07-13 11:42:44 +0900

    w서버 통합으로 인한 미사용

--- Cogs/ingame.py
+++ Cogs/ingame.py
@@ -1,4 +1,4 @@
-from discord.ext import commands
+""" from discord.ext import commands
 from discord import app_commands, Interaction
 import requests
 from bs4 import BeautifulSoup
@@ -27,3 +27,4 @@ async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(
         Ingame(bot)
     )
+ """
