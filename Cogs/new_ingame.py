ddc284d394cac91db6b0f54ceaea3b0d46e66225
21in7 <skawksmsduzo@gmail.com>
2023-07-18 15:59:05 +0900

    글오세버전 동접자 추가

--- Cogs/new_ingame.py
+++ Cogs/new_ingame.py
@@ -9,19 +9,24 @@ class New_Ingame(commands.Cog):
         self.bot = bot
 
     @app_commands.command(
-        name="동접자", description="jtos ingame player + ktos ingame player"
+        name="동접자",
+        description="jtos ingame player + ktos ingame player + itos ingame player",
     )
     async def in_game(self, interaction: Interaction):
         # 일본 서버 접속자
         jpn_urls = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key={API_KEY.steam_api}&appid=1175730"
         # 한국 서버 접속자
         kor_urls = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key={API_KEY.steam_api}&appid=2178420"
+        itos_urls = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key={API_KEY.steam_api}&appid=372000"
         kor_response = requests.get(url=kor_urls).json()
         jpn_response = requests.get(url=jpn_urls).json()
+        itos_response = requests.get(url=itos_urls).json()
         # 일본섭 + 한국섭 접속자
-        sum = jpn_response.get("response").get("player_count") + kor_response.get(
-            "response"
-        ).get("player_count")
+        sum = (
+            jpn_response.get("response").get("player_count")
+            + kor_response.get("response").get("player_count")
+            + itos_response.get("response").get("player_count")
+        )
 
         await interaction.response.send_message(
             str(sum)
@@ -29,6 +34,8 @@ class New_Ingame(commands.Cog):
             + str(kor_response.get("response").get("player_count"))
             + "\njtos : "
             + str(jpn_response.get("response").get("player_count"))
+            + "\nitos : "
+            + str(itos_response.get("response").get("player_count"))
         )
 
 
